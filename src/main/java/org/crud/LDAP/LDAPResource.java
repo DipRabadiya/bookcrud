0d1f59595998d5796176baf4886960f13484cf56
DipRabadiya27 <diprabadiya@gmail.com>
2024-05-28 16:32:14 +0530

    add faulttolerance

--- src/main/java/org/crud/LDAP/LDAPResource.java
+++ src/main/java/org/crud/LDAP/LDAPResource.java
@@ -4,8 +4,16 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.crud.Service.BookService;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.faulttolerance.Timeout;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Metered;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
+import java.time.temporal.ChronoUnit;
 import java.util.logging.Logger;
 
 @Path("/")
@@ -18,14 +26,24 @@ public class LDAPResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Fallback(fallbackMethod = "getAllUserFallback")
+//    @Timeout(1000)
+//    @Retry(maxRetries = 3)
+    @CircuitBreaker(requestVolumeThreshold = 4, failureRatio = 0.5, delay = 10, delayUnit = ChronoUnit.SECONDS)
+
     public Response getAllUser() {
         LOGGER.info("Fetching all users from LDAP");
         return Response.ok(ldapRestClient.getAllUsers()).build();
     }
 
+    public Response getAllUserFallback(){
+        return Response.ok("Server is down, Please wait").build();
+    }
+
     @POST
     @Path("/user")
     @Consumes(MediaType.APPLICATION_JSON)
+    @Fallback(fallbackMethod = "addUserFallback")
     public Response addUser(User user) {
         LOGGER.info("Adding new user to LDAP: " + user.getCn());
         Response response = ldapRestClient.addUser(user);
@@ -33,8 +51,13 @@ public class LDAPResource {
         return response;
     }
 
+    public Response addUserFallback(User user){
+        return Response.ok("Server is down, Please wait").build();
+    }
+
     @DELETE
     @Path("/user/{cn}")
+    @Fallback(fallbackMethod = "deleteUserFallback")
     public Response deleteUser(@PathParam("cn") String cn) {
         LOGGER.info("Deleting user from LDAP: " + cn);
         Response response = ldapRestClient.deleteUser(cn);
@@ -42,9 +65,14 @@ public class LDAPResource {
         return response;
     }
 
+    public Response deleteUserFallback(String cn){
+        return Response.ok("Server is down, Please wait").build();
+    }
+
     @POST
     @Path("/user/auth")
     @Consumes(MediaType.APPLICATION_JSON)
+    @Fallback(fallbackMethod = "authUserFallback")
     public Response authUser(User user) {
         LOGGER.info("Authenticating user: " + user.getCn());
         Response response = ldapRestClient.authUser(user);
@@ -52,9 +80,14 @@ public class LDAPResource {
         return response;
     }
 
+    public Response authUserFallback(User user){
+        return Response.ok("Server is down, Please wait").build();
+    }
+
     @PUT
     @Path("/user/password")
     @Consumes(MediaType.APPLICATION_JSON)
+    @Fallback(fallbackMethod = "updateUserPasswordFallback")
     public Response updateUserPassword(User user) {
         LOGGER.info("Updating password for user: " + user.getCn());
         Response response = ldapRestClient.updateUserPassword(user);
@@ -62,9 +95,14 @@ public class LDAPResource {
         return response;
     }
 
+    public Response updateUserPasswordFallback(User user){
+        return Response.ok("Server is down, Please wait").build();
+    }
+
     @PUT
     @Path("/user/details")
     @Consumes(MediaType.APPLICATION_JSON)
+    @Fallback(fallbackMethod = "updateUserDetailsFallback")
     public Response updateUserDetails(User user) {
         LOGGER.info("Updating details for user: " + user.getCn());
         Response response = ldapRestClient.updateUserDetails(user);
@@ -72,13 +110,22 @@ public class LDAPResource {
         return response;
     }
 
+    public Response updateUserDetailsFallback(User user){
+        return Response.ok("Server is down, Please wait").build();
+    }
+
     @GET
     @Path("/check")
     @Produces(MediaType.TEXT_PLAIN)
+    @Fallback(fallbackMethod = "checkConnectionFallback")
     public Response checkConnection() {
         LOGGER.info("Checking LDAP connection");
         Response response = ldapRestClient.checkConnection();
         LOGGER.info("LDAP connection status: " + response.getStatus());
         return response;
+    }
+
+    public Response checkConnectionFallback(){
+        return Response.ok("Server is down, Please wait").build();
     }
 }
